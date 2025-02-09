e1a5f9f9602d389297456b82cbd69f7efe718d96
wnkinc <140204064+wnkinc@users.noreply.github.com>
2025-02-09 14:40:18 -0800

    Style filter for feed

--- src/public/scripts/feed-filter.js
+++ src/public/scripts/feed-filter.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const tabs = document.querySelectorAll(".tab");
+  const tabs = document.querySelectorAll(".filter-tab");
   const postsContainer = document.querySelector(".feed");
   const posts = Array.from(document.querySelectorAll(".feed-post"));
 
